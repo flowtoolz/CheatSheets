7512cbf6a45e877e3516608f6ef72fa74455faed
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2023-08-02 19:42:28 +0200

    Add script for restoring Flowlist data from backup in iCloud

--- shell-customization-scripts/personalize-the-shell.sh
+++ shell-customization-scripts/personalize-the-shell.sh
@@ -10,13 +10,25 @@ alias cdc="cd '$cloud'"
 export repos="/Users/seb/Desktop/GitHub Repos"
 alias cdr="cd '$repos'"
 
-# backup Flowlist (http://www.flowlistapp.com)
+# backup and restore Flowlist (http://www.flowlistapp.com)
+
 bfl() {
     # define what to backup
     sourceFolder="/Users/seb/Library/Containers/com.flowtoolz.flowlist/Data/Documents/Flowlist-Beta/"
 
     # define where to store the backup
     destinationFolder="$cloud/FLOWLIST BACKUP/Flowlist-Beta/"
+
+    # update the destination folder, deleting files that aren't anymore in the source folder
+    rsync -a --delete $sourceFolder $destinationFolder
+}
+
+rfl() {
+    # define where the backup is stored
+    sourceFolder="$cloud/FLOWLIST BACKUP/Flowlist-Beta/"
+
+    # define what to restore
+    destinationFolder="/Users/seb/Library/Containers/com.flowtoolz.flowlist/Data/Documents/Flowlist-Beta/"
 
     # update the destination folder, deleting files that aren't anymore in the source folder
     rsync -a --delete $sourceFolder $destinationFolder
